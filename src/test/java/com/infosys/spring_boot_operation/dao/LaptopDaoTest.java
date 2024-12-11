9eaa72910a3ff47ada2c28958f68797588ed4437
Rahul <newrahulgautam@gmail.com>
2024-12-11 19:08:33 +0530

    Update24.12.11.01

---
+++ src/test/java/com/infosys/spring_boot_operation/dao/LaptopDaoTest.java
@@ -0,0 +1,14 @@
+package com.infosys.spring_boot_operation.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class LaptopDaoTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
